705f6eb4bfa44207d86b260ad1faa8b39387aab9
maurya-sudhanshu <sudhanshumaurya2000@gmail.com>
2023-04-06 16:21:27 +0530

    Edit by sudhanshu

--- src/user-api-folder/user.js
+++ src/user-api-folder/user.js
@@ -75,6 +75,7 @@ app.post("/register", (req, res) => {
   );
 });
 app.post("/login", (req, res) => {
+  console.log("");
   con.query(
     "select * from user_details where user_name=?",
     [req.body.mobile],
@@ -794,7 +795,7 @@ app.post("/get-statement", verifytoken, (req, res) => {
   })
 });
 app.post("/get-match", verifytoken, (req, res) => {
-  con.query("SELECT m.id,t1.team_name as team1_name,t1.short_name,t2.team_name as team2_name,t2.short_name,s.series_name,m.team1_id,m.team2_id,m.series_id,m.result,m.status,m.match_date FROM `match` as m INNER join teams as t1 on m.team1_id = t1.id INNER join teams as t2 on m.team2_id = t2.id INNER join series as s on s.id = m.series_id", (err, result) => {
+  con.query("SELECT m.id,t1.team_name as team1_name,t1.short_name as short1_name,t2.team_name as team2_name,t2.short_name  as short2_name,s.series_name,m.team1_id,m.team2_id,m.series_id,m.result,m.status,m.match_date FROM `match` as m INNER join teams as t1 on m.team1_id = t1.id INNER join teams as t2 on m.team2_id = t2.id INNER join series as s on s.id = m.series_id", (err, result) => {
     if (err) throw err;
     else {
       res.status(200).send({ data: result });
